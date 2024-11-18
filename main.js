26afa62a50c2d90012594ff42eaf3a9b99ed899d
Firas Jebri <fiiraas.jeebrii@gmail.com>
2024-11-18 15:41:41 +0100

    Adding the version 0 from a previous sprint

---
+++ main.js
@@ -0,0 +1,195 @@
+var product1 = {
+    name: "Smartphone XIAOMI Redmi A3x",
+    description: "Double SIM - Écran Dot Drop LCD IPS 6.71' (1650 x 720 px), 90 Hz - Processeur Unisoc T603, (22 nm, octa-core, jusqu'à 1.8 GHz) - RAM 3 Go - Stockage 64 Go - Caméra arrière principale: 8 MP - Caméra frontale: 5 MP - Batterie 5000 mAh - Charge USB Type-C 10 W - Capteur d'empreintes digitales latéral - Déverrouillage du visage par l'IA - prend en charge microSD - Réseau 4G - Wi-Fi - Bluetooth 4.2 - Prise casque 3.5 mm - Système opérateur: Android 14 - Couleur Blanc - Garantie 1 an",
+    ram: 3,
+    storage: 64,
+    camera: 8,
+    price: 329.000,
+    disponibility: false,
+    imgUrl: "https://www.tunisianet.com.tn/385976-home/smartphone-xiaomi-redmi-a3x-3-go-64-go-blanc.jpg",
+    brand: "xiaomi"
+}
+var product2 = {
+    name: "Smartphone XIAOMI Redmi 13",
+    description: "Écran FHD + 6.79' (2460 x 1080 pixels) - Taux de rafraîchissement : jusqu'à 90 Hz - Processeur Mediatek Helio G91 Ultra (12 nm) Octa-core - RAM 6Go - Mémoire 128Go - Appareil photo principal 108MP ultra-net - Caméra macro 2MP - Caméra frontale 13MP - Batterie 5030 mAh Type-C - Charge rapide 33W - Capteur d'empreintes digitales latéral - Déverrouillage du visage par l'IA - Résistant à la poussière et aux éclaboussures IP53 - Wi-Fi - Bluetooth5.3 - Prise casque 3.5 mm - Couleur Bleu - Garantie 1 an ",
+    ram: 6,
+    storage: 128,
+    camera: 108,
+    price: 599.000,
+    disponibility: true,
+    imgUrl: "https://www.tunisianet.com.tn/382948-large/smartphone-xiaomi-redmi-13-6go-128go-bleu.jpg",
+    brand: "xiaomi"
+}
+var product3 = {
+    name: "Smartphone SAMSUNG GALAXY A15",
+    description: "Double SIM - Écran Dot Drop LCD IPS 6.71' (1650 x 720 px), 90 Hz - Processeur Unisoc T603, (22 nm, octa-core, jusqu'à 1.8 GHz) - RAM 3 Go - Stockage 64 Go - Caméra arrière principale: 8 MP - Caméra frontale: 5 MP - Batterie 5000 mAh - Charge USB Type-C 10 W - Capteur d'empreintes digitales latéral - Déverrouillage du visage par l'IA - prend en charge microSD - Réseau 4G - Wi-Fi - Bluetooth 4.2 - Prise casque 3.5 mm - Système opérateur: Android 14 - Couleur Blanc - Garantie 1 an",
+    ram: 4,
+    storage: 128,
+    camera: 50,
+    price: 639.000,
+    disponibility: true,
+    imgUrl: "https://www.tunisianet.com.tn/397721-large/smartphone-samsung-galaxy-a15-4128g-bleu-noir.jpg",
+    brand: "samsung"
+}
+var product4 = {
+    name: "Smartphone VIVO Y28",
+    description: "Double SIM - Écran LCD 6.68' LED (1608 x 720 pixels), 90 Hz - Processeur Helio G85 (8 cœurs, 12 nm, 2× 2.0 GHz + 6× 1.8 GHz) - RAM 16 Go (Ram extensible 8Go + 8 Go) - Stockage 256 Go - Caméra: Avant 8 MP / Arrière 50 MP + 2 MP - Flash arrière - Batterie Li-ion 6000 mAh - Charge Rapide 44W - Capteur d'empreintes digitales capacitif latéral - Réseau 4G - Connectivité: Wi-Fi + Bluetooth 5.0 - Système d'exploitation: Android 14 - Indice de protection IP: IP64 (Résistant à l’eau et à la poussière) - Couleur Orange - Garantie 1 an",
+    ram: 16,
+    storage: 256,
+    camera: 50,
+    price: 699.000,
+    disponibility: true,
+    imgUrl: "https://www.tunisianet.com.tn/377455-large/vivo-y28-prix-tunisie.jpg",
+    brand: "others"
+}
+var product5 = {
+    name: "Smartphone XIAOMI Redmi Note 13 PRO",
+    description: "Double SIM - Ecran 6.67' Amoled 120 Hz (1080 x 2400px) - Processeur Helio G99 Ultra Octa Core Cadencé à 2.2 Ghz - RAM 8Go - Stockage 256Go - Android 13 avec MIUI14 -  Appareil Photo arrière : capteur principal de 200MP + capteur ultra grand angle de 8MP + caméra macro de 2MP, vidéo 1080p, Intelligence Artificielle - Appareil Photo Avant 16MP - Bluetooth - GPS - Wifi - lecteur d'empreintes sous l'écran - reconnaissance faciale - Batterie 5000 mAh avec charge rapide de 67W - Couleur Noir - Garantie 1 an",
+    ram: 8,
+    storage: 256,
+    camera: 200,
+    price: 999.000,
+    disponibility: true,
+    imgUrl: "https://www.tunisianet.com.tn/353403-large/smartphone-xiaomi-redmi-note-13-pro-8go-256go-noir.jpg",
+    brand: "xiaomi"
+}
+var product6 = {
+    name: "Smartphone XIAOMI Redmi Note 13 PRO+ 5G",
+    description: "Double SIM - Écran 6.67', AMOLED 1.5K, 1220 x 2712 pixels, taux de rafraîchissement 120 Hz, taux d'échantillonnage jusqu'à 240 Hz, 500 nits - Corning Gorilla Glass Victus - Processeur MediaTek Dimensity 7200 Octo-Core cadencé à 2.8 GHz - Taux de rafraîchissement 120Hz - RAM 12Go - Mémoire 512Go (extensible jusqu'à 1 To via carte microSDXC) - Système: Android 13 avec MIUI14 - Appareils photo principal : Triple caméra: Caméra principale 200MP, Caméra ultra grand-angle 8MP, Objectif macro 2MP - Caméra frontale 16MP - Batterie 5100 mAh Type-C - Charge Rapide 120W - Lecteur d'empreintes sous l'écran - Wi-Fi - GPS - Reconnaissance faciale -  Couleur Violet - Garantie 1 an",
+    ram: 12,
+    storage: 512,
+    camera: 200,
+    price: 1869.000,
+    disponibility: true,
+    imgUrl: "https://www.tunisianet.com.tn/353231-large/smartphone-xiaomi-redmi-note-13-pro-8go-256go-purple.jpg",
+    brand: "xiaomi"
+}
+var product7 = {
+    name: "Smartphone Apple iPhone 13 5G",
+    description: "Double SIM - Ecran Super Retina XDR OLED 6.1' - Résolution 1170 x 2532 px - Processeur Apple A15 Bionic Hexa-Core - RAM 4 Go - Stockage 128 Go - 2x Appareils Photo Arrière 12 Mégapixels (grand‑angle et ultra grand‑angle Grand‑angle: ouverture F/1,6 Ultra grand‑angle : ouverture F/2,4 et champ de vision de 120°) -  Système iOS 15 - 5G - Wifi - NFC - Bluetooth 5.0 - Wi-Fi 6 AX - Dimensions 146.7 x 71.5 x 7.65 mm - Garantie 1 an",
+    ram: 4,
+    storage: 128,
+    camera: 12,
+    price: 3199.000,
+    disponibility: true,
+    imgUrl: "https://www.tunisianet.com.tn/349181-large/smartphone-apple-iphone-13-5g-4-go-128-go-starlight.jpg",
+    brand: "apple"
+}
+var product8 = {
+    name: "Smartphone Samsung Galaxy S24 5G",
+    description: "Double SIM - Écran 6.2' AMOLED dynamique 2X - Résolution: Full HD+ (2340 x 1080) - Taux de rafraîchissement: 120 Hz - Processeur: Samsung Exynos 2400 Dix cœurs - Mémoire: 8 Go - Stockage: 256 Go - Résolution caméra arrière (Multiple): 50.0 MP (f1.8) + 10.0 MP (f2.4) + 12.0 MP (f2.2) - Auto Focus - Digital Zoom up to 30x - Résolution caméra frontale: 12.0 MP (f/2.2) - Auto Focus caméra frontale - Flash - Réseau 5G - Connectivité: Interface USB-C USB 3.2 Gen 1 - Technologie de localisation GPS - Wi-Fi - Bluetooth 5.3 - NFC - Système d'exploitation: Android 14 - Capacité de la batterie: 4000 mAh - Haut-parleurs stéréo - Poids: 167 g - Couleur Gris - Garantie 1 an",
+    ram: 8,
+    storage: 256,
+    camera: 50,
+    price: 3699.000,
+    disponibility: true,
+    imgUrl: "https://www.tunisianet.com.tn/351797-large/smartphone-samsung-galaxy-s24-5g-8-go-256-go-silver.jpg",
+    brand: "samsung"
+}
+var product9 = {
+    name: "Apple iPhone 16 5G Ultramarine",
+    description: "Double SIM (Nano-SIM, eSIM) - Ecran HDR Super Retina XDR OLED 6.1' (Résolution 1179 x 2556px) - Processeur Apple A18 Hexa-Core - Mémoire 128 Go - Système Apple iOS 18 - Wi-Fi 7 (802.11be) - Bluetooth 5.3 - 5G - Appareil Photo 48 Megapilxels (Grand Angle) + 12 Megapixels (Ultra Grand Angle) + 12 Megapixels (telephoto) (arrières), 12 Megapixels f/1,9 (avant) - Etanchéité IP68 - Reconnaissance Faciale - Dimensions 147.6 x 71.6 x 7.8 mm - Poids 170g - Garantie 1 an",
+    ram: 8,
+    storage: 128,
+    camera: 48,
+    price: 4929.000,
+    disponibility: false,
+    imgUrl: "https://www.tunisianet.com.tn/395439-large/apple-iphone-16-128-go-5g-ultramarine.jpg",
+    brand: "apple"
+}
+var product10 = {
+    name: "Smartphone Samsung Galaxy Z FOLD 6 Silver Shadow",
+    description: "Smartphone Samsung Galaxy Z FOLD 6 - Ecran principal 7.6' AMOLED dynamique 2X, 2160 x 1856 pixels - Ecran externe : 6.3' AMOLED dynamique 2X, 120Hz, 2376 x 968 pixels - Verre Corning Gorilla Victus 2 - Système d'exploitation: Android 14 One UI 6.1.1 - Processeur : Snapdragon 8 Gen 3 (4nm) - Stockage : 256 Go - Mémoire RAM : 12Go - Caméra arrière : 50 MP + 12MP + 10MP - zoom numérique jusqu'à 30x - Caméra avant : 10MP/4MP Caméra sous écran - Vidéo : UHD 8K (7680 x 4320px) - AI : Circle to search, Live Translate, Transcript assist, photo assist, interpreter assist - Note assist - Haut-Parleurs Stéréo - Double sim - Connectivité: WiFi 5G, Bluetooth 5.3 - NFC - Lecteur d'empreinte digitale - Batterie: 4400 mAh - Charge rapide : 25W - Résistant à l'eau: IP48 - Interface : USB Type C - Couleur : Silver Shadow - Garantie 1 an",
+    ram: 12,
+    storage: 256,
+    camera: 50,
+    price: 7999.000,
+    disponibility: false,
+    imgUrl: "https://www.tunisianet.com.tn/397266-large/smartphone-samsung-galaxy-z-fold-6-12-go-256-go-silver-shadow.jpg",
+    brand: "samsung"
+}
+var products = [product1, product2, product3, product4, product5, product6, product7, product8, product9, product10];
+
+$("document").ready(function () {
+
+    //function to display according to the brand
+    function displayBrand(brand) {
+        for (let i = 0; i < products.length; i++) {
+            var brand1 = brand;
+            if (brand1 === "all") {
+                brand1 = products[i].brand;
+            }
+            if (products[i].brand === brand1) {
+                var stock = "Not In Stock";
+                if (products[i].disponibility) {
+                    stock = "In Stock";
+                }
+
+                $("#phones").append("<div class='card' style='width: 18rem;'><img class='card-img-top' src= "
+                    + products[i].imgUrl
+                    + " alt='Card image cap'><div class='card-body'><h5 class='card-title'>"
+                    + products[i].name
+                    + "</h5><p class='card-text'>"
+                    + products[i].description
+                    + "</p></div><ul class='list-group list-group-flush'>"
+                    + "<li class='list-group-item'><b>RAM : </b>"
+                    + products[i].ram
+                    + " Go</li>"
+                    + "<li class='list-group-item'><b>Storage : </b>"
+                    + products[i].storage
+                    + " Go</li>"
+                    + "<li class='list-group-item'><b>Camera : </b>"
+                    + products[i].camera
+                    + " px</li>"
+                    + "<li class='list-group-item'><b>Price : </b>"
+                    + products[i].price
+                    + " DT</li>"
+                    + "<li class='list-group-item'><b>Disponibility : </b>"
+                    + stock
+                    + "</li></ul>"
+                    + "<div class='card-body'><a href='#' class='card-link'>More Info</a>"
+                    + "<a href='#' class='card-link' id='add-cart'>Add To Card</a>"
+                    + "</div></div>"
+                );
+                //Deactivate button add to cart for those who not in stock
+                if (products[i].disponibility) {
+                    $("#add-cart").css( "pointer-events", "none" ); // Doesn't work
+                }
+            }
+        }
+    }
+
+    displayBrand("all");
+
+    //Display All The Products
+    $("#all-filter").click(function() {
+        $("#phones").empty();
+        displayBrand("all");
+    });
+
+    //Display Only Apple Phones
+    $("#apple-filter").click(function () {
+        $("#phones").empty();
+        displayBrand("apple");
+    });
+
+    //Display Only Samsung Phones
+    $("#samsung-filter").click(function () {
+        $("#phones").empty();
+        displayBrand("samsung");
+    });
+
+    //Display Only Xiaomi Phones
+    $("#xiaomi-filter").click(function () {
+        $("#phones").empty();
+        displayBrand("xiaomi");
+    });
+
+    //Display Only Others Phones
+    $("#others-filter").click(function () {
+        $("#phones").empty();
+        displayBrand("others");
+    });
+
+});
+
